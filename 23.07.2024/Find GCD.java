ddfe9fbf565938391c1de6611b46b1a844f42db9
Mounika-Selvaraj <146700795+Mounika-Selvaraj@users.noreply.github.com>
2024-07-23 14:18:54 +0530

    Create  Find GCD.java

---
+++ 23.07.2024/Find GCD.java
@@ -0,0 +1,13 @@
+public class GCD {
+    public static void main(String[] args) {
+        int num1 = 56, num2 = 98;
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + findGCD(num1, num2));
+    }
+
+    public static int findGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return findGCD(b, a % b);
+    }
+}
