2eb2bb2469d94e0bc50a0319b0b3febd8f73cb5c
Mounika-Selvaraj <146700795+Mounika-Selvaraj@users.noreply.github.com>
2024-07-23 14:24:20 +0530

    Create Count Characters .java

---
+++ 23.07.2024/Count Characters .java
@@ -0,0 +1,14 @@
+public class CountCharacters {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                count++;
+            }
+        }
+
+        System.out.println("Number of characters in the string: " + count);
+    }
+}
